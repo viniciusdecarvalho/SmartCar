3a773336e24a3e3c14d5709465ba39206ce07e3b
Vinicius de Carvalho <viniciusdecarvalho.tec@gmail.com>
2020-03-22 23:21:12 -0300

    criado projeto para ios

---
+++ SmartCar-ios/SmartCar/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  SmartCar
+//
+//  Created by Vinicius Carvalho on 22/03/20.
+//  Copyright © 2020 Vinicius Carvalho. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var selection = 0
+ 
+    var body: some View {
+        TabView(selection: $selection){
+            Text("First View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("first")
+                        Text("First")
+                    }
+                }
+                .tag(0)
+            Text("Second View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("second")
+                        Text("Second")
+                    }
+                }
+                .tag(1)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
